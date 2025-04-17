4c920aa2112cd687028cd3af37377bf02a3427bb
pratyush <pratyush.singh@glingtech.onmicrosoft.com>
2025-04-17 11:04:08 +0530

    fixed cancel button font and removed unnecessary code from grid sys comp

--- src/app/grid-system/grid-system.component.ts
+++ src/app/grid-system/grid-system.component.ts
@@ -1,9 +1,7 @@
 import { Component, inject, Input, OnInit } from '@angular/core';
 import { DragDropModule, CdkDragDrop, copyArrayItem, moveItemInArray } from '@angular/cdk/drag-drop';
-// import { MatDialog } from '@angular/material/dialog';
 
 import { SidebarComponent } from "../sidebar/sidebar.component";
-// import { MymodalComponent } from '../mymodal/mymodal.component';
 import { WorkflowService } from '../workflow.service';
 import { MaterialModule } from '../material.module';
 import { DropboxComponent } from "../dropbox/dropbox.component";
@@ -42,42 +40,4 @@ export class GridSystemComponent implements OnInit {
     const totalInnerItems = this.innerGridSize * this.innerGridSize;
     this.innerGridItems = Array.from({ length: totalInnerItems }, (_, i) => i + 1);
   }
-
-  // drop(event: CdkDragDrop<string[]>) {
-  //   if (event.previousContainer === event.container) {
-  //     moveItemInArray(
-  //       event.container.data,
-  //       event.previousIndex,
-  //       event.currentIndex
-  //     );
-  //   } else {
-  //     copyArrayItem(
-  //       event.previousContainer.data,
-  //       event.container.data,
-  //       event.previousIndex,
-  //       event.currentIndex
-  //     );
-  //   }
-  // }
-
-  // getFormattedOperator(operator: string): string {
-  //   return this.workflowService.getFormatted(operator);
-  // }
-
-  // openModal(dropItem: any): void {
-  //   this.comp = 'operator'
-  //   this.dialog.open(MymodalComponent, {
-  //     width: "800px", 
-  //     maxWidth: '90vw',  
-  //     data: { dropItem },
-  //   });
-  // }
-
-
-  // removeItem(index: number){
-  //   this.droppedItems.splice(index, 1);
-  //   // this.dropedItem = [...this.dropedItem]; it forces Angular to detect changes by creating a new reference to the array.
-  //   // when using functions like filter(), it returns a entierly new array which force angular change detection so there is no use of this.dropedItem = [...this.dropedItem]; 
-  //   // Normally, Angular's change detection doesn't detect in-place mutations (using .splice(), .push(), .pop()), but in your case: The component si inside a cdkDropList, which triggers change detection automatically.
-  // }
 }
